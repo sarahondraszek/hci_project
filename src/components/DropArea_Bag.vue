f3e533dabc2111556e2bfec2b6d621766581cf45
kisram <s2kisram@uni-trier.de>
2022-02-03 23:59:19 +0100

    Prep for drag&drop & some cleanup

---
+++ src/components/DropArea_Bag.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card class="pa-md-4" color="dark-grey">
+    <v-img src="@/assets/book-bag.png" contain></v-img>
+    <v-card-actions class="justify-center">
+      <router-link style="text-decoration: none; color: inherit;" to="/bookbag">
+        <v-btn
+            outlined
+            rounded
+            text
+            color="orange accent-4"
+        >
+          View Bookbag
+        </v-btn>
+      </router-link>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "DropArea_Bag"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Student.vue
+++ src/views/Student.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-row>
+    <v-row dense>
       <v-col cols="10">
         <v-card class="mx-auto"
                 max-width="900">
@@ -9,65 +9,104 @@
               dark
           >
             <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
             <v-toolbar-title>Courses Winter 2022</v-toolbar-title>
             <v-spacer></v-spacer>
           </v-toolbar>
 
-          <draggable class="row" :sort="true">
-            <v-col>
-              <Course courseID="BWL-001" title="Einführung in die BWL" teacher="Mueller" location="Audimax"
-                      time="Mo, 10:15" sws="5"/>
-            </v-col>
-            <v-col>
-              <Course courseID="BWL-002" title="Einführung in die VWL" teacher="Haupt" location="A18"
-                      time="Di, 14:15" sws="5"/>
-            </v-col>
-            <v-col>
-              <Course courseID="BWL-003" title="Mathe 1" teacher="Drews" location="B35"
-                      time="Do, 12:15" sws="10"/>
-            </v-col>
-          </draggable>
+            <Course v-for="course in listOne"
+                    :key="course.courseID"
+                    :courseID='course.courseID'
+                    :title='course.title'
+                    :teacher='course.teacher'
+                    :location='course.location'
+                    :time='course.time'
+                    :sws='course.sws'
+                    draggable
+                    @dragstart="startDrag($event, item)"/>
         </v-card>
+
       </v-col>
       <v-col cols="2">
-        <v-card class="pa-md-4" color="dark-grey">
-          <v-img src="@/assets/book-bag.png" contain></v-img>
-          <v-card-actions class="justify-center">
-            <router-link style="text-decoration: none; color: inherit;" to="/bookbag">
-              <v-btn
-                  outlined
-                  rounded
-                  text
-                  color="orange accent-4"
-              >
-                View Bookbag
-              </v-btn>
-            </router-link>
-          </v-card-actions>
-        </v-card>
+        <DropArea_Bag
+            @drop="onDrop($event,2)"
+            @dragover.prevent
+            @dragenter.prevent/>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-//import { mapGetters, mapMutations } from "vuex";
-import draggable from "vuedraggable";
 import Course from "@/components/Course";
-
+import DropArea_Bag from "@/components/DropArea_Bag";
 export default {
   name: "Student",
   components: {
-    draggable,
-    Course,
+    DropArea_Bag,
+    Course
   },
   data() {
-    return {}
+    return {
+      courses:[{
+        courseID: 'BWL-001',
+        title: 'Einführung in die BWL',
+        teacher: "Mueller",
+        location: "Audimax",
+        time: "Mo, 10:15",
+        sws: 5,
+        list:1
+      },
+        {courseID: "BWL-002",
+        title: "Einführung in die VWL",
+          teacher:"Haupt",
+          location: "A18",
+          time: "Di, 14:15",
+          sws:5,
+          list:1},
+        {
+        courseID: "BWL-003",
+          title: "Mathe 1",
+          teacher: "Drews",
+          location: "B35",
+          time: "Do, 12:15",
+          sws: 10,
+          list:1
+        }]
+    }
   },
-};
+computed: {
+  listOne(){
+    return this.courses.filter(item => item.list === 1)
+  },
+  listTwo(){
+    return this.courses.filter(item => item.list === 2)
+  }
+  },
+  methods: {
+    startDrag (evt, item) {
+      evt.dataTransfer.dropEffect = 'move'
+      evt.dataTransfer.effectAllowed = 'move'
+      evt.dataTransfer.setData('itemID', item.courseID)
+    },
+    onDrop (evt, list) {
+      const itemID = evt.dataTransfer.getData('itemID')
+      const item = this.courses.find(item => item.courseID == itemID)
+      item.list = list
+      console.log(itemID)
+    }
+  }
+}
 </script>
 
 <style scoped>
-
+.drop-zone{
+  background-color: #eee;
+  margin-bottom: 10px;
+  padding: 10px;
+}
+.drag-el {
+  background-color:#fff;
+  margin-bottom: 10px;
+  padding: 5px;
+}
 </style>
